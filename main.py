5b4f8770d8f5340275b42f561ec3a043116f96f7
Tyler Dubuke <tgdubuke@wpi.edu>
2020-04-30 17:03:38 -0400

    algorithm for prediction based on cosine similarity

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import numpy as np
 import parse as ps
+import sim_train as st
 
 # Main code (do stuff here)
 if __name__ == "__main__":
     x_train, y_train, ing, c_train, ids_train = ps.load("train")
     x_test, _, _, _, ids_test = ps.load("test")
 
-    print(ing)
+    predictions = st.train_correlation(x_train, ing, c_train)
+    y_train_list = []
+    for el in y_train:
+        y_train_list.append(''.join(str(e) for e in st.un_onehot(el, c_train)))
+    acc = st.accuracy_corrolation(predictions, y_train_list)
+    print("Accuracy: {}%".format(acc * 100))
